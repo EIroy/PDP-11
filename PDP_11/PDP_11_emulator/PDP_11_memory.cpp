99323db2782b766fa5e3a9576be4917d073de3aa
DeliteI <vladislav.fofanov@frtk.ru>
2015-09-30 19:13:27 +0300

    Add memory functional to project

---
+++ PDP_11/PDP_11_emulator/PDP_11_memory.cpp
@@ -0,0 +1,30 @@
+#include "PDP_11_memory.h"
+#include <malloc.h>
+
+status_t PDP_11_memory_init(PDP_11_memory* memory) {
+	memory->start_pos = malloc(MEMORY_SIZE);
+	memory->start_RAM_pos = memory->start_pos;
+	memory->start_VRAM_pos = (void*)((int)memory->start_RAM_pos + RAM_SIZE);	//
+	memory->start_ROM_pos = (void*)((int)memory->start_VRAM_pos + VRAM_SIZE);	// @TODO: improve addreses initialize
+	memory->start_IO_pos = (void*)((int)memory->start_ROM_pos + ROM_SIZE);		//
+
+	memory->all_memory_size = MEMORY_SIZE;
+	memory->RAM_size = RAM_SIZE;
+	memory->VRAM_size = VRAM_SIZE;
+	memory->ROM_size = ROM_SIZE;
+	memory->IO_size = IO_SIZE;
+
+	return 0;
+}
+
+status_t PDP_11_memory_read_byte(addres_t addr, PDP_11_memory* memory, byte_t* buffer) {
+	byte_t* abs_addr = (byte_t*)((int)memory->start_pos + (int)addr);
+	*buffer = *abs_addr;
+	
+	return 0;
+}
+
+status_t PDP_11_memory_write_byte(addres_t addr, PDP_11_memory* memory, byte_t buffer) {
+	byte_t* abs_addr = (byte_t*)((int)memory->start_pos + (int)addr);
+	*abs_addr = buffer;
+}
---
+++ PDP_11/PDP_11_emulator/PDP_11_memory.h
@@ -0,0 +1,32 @@
+#define MEMORY_SIZE 0x8000	//
+#define RAM_SIZE	0x2000	//
+#define VRAM_SIZE	0x2000	// maybe move to other file
+#define ROM_SIZE	0x3C00	//
+#define IO_SIZE		0x0400	//
+
+typedef int size_t;
+typedef int status_t;
+typedef short addres_t;
+typedef unsigned char byte_t;
+
+typedef struct _PDP_11_memory {
+	void* start_pos;
+
+	void* start_RAM_pos;
+	void* start_VRAM_pos;
+	void* start_ROM_pos;
+	void* start_IO_pos;
+
+	size_t all_memory_size;
+
+	size_t RAM_size;
+	size_t VRAM_size;
+	size_t ROM_size;
+	size_t IO_size;
+
+	size_t ROM_loader_sec_size; //locate in the beginnig of ROM
+} PDP_11_memory;
+
+status_t PDP_11_memory_init(PDP_11_memory* memory);
+status_t PDP_11_memory_read_byte(addres_t addr, PDP_11_memory* memory, byte_t* buffer);
+status_t PDP_11_memory_write_byte(addres_t addr, PDP_11_memory* memory, byte_t buffer);
